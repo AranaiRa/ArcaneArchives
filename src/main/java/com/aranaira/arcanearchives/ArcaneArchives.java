5926495c5222c4c4a73aeca27478a6bead9b9fe2
Lenyeto <patg.lenyeto@gmail.com>
2018-09-21 15:49:45 -0400

    Initial Commit

---
+++ src/main/java/com/aranaira/arcanearchives/ArcaneArchives.java
@@ -0,0 +1,31 @@
+package com.aranaira.arcanearchives;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = ArcaneArchives.MODID, name = ArcaneArchives.NAME, version = ArcaneArchives.VERSION)
+public class ArcaneArchives
+{
+    public static final String MODID = "arcanearchives";
+    public static final String NAME = "Arcane Archives";
+    public static final String VERSION = "0.1";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
