a7f72ba3ab11e2b6e7bd2c73aec7a5bd751fc845
AranaiRa <aranaiseraph@gmail.com>
2018-09-23 07:01:51 -0400

    2018/09/23A
    Added variables to ImmanenceTileEntity and RadiantResonatorTileEntity

--- src/main/java/com/aranaira/arcanearchives/tileentities/ImmanenceTileEntity.java
+++ src/main/java/com/aranaira/arcanearchives/tileentities/ImmanenceTileEntity.java
@@ -4,6 +4,9 @@ import net.minecraft.tileentity.TileEntity;
 
 public class ImmanenceTileEntity extends TileEntity 
 {
-	public int ImmanenceDrain;
-	
+	public String NetworkUUID; //UUID of network owner
+	public int ImmanenceDrain; //Immanence cost to operate the device
+	public int NetworkPriority; //What order the device's Immanence is paid for
+	public boolean IsDrainPaid; //Whether the device's Immanence needs have been covered
+	public boolean IsProtected; //Whether the device is currently indestructable
 }
--- src/main/java/com/aranaira/arcanearchives/tileentities/RadiantResonatorTileEntity.java
+++ src/main/java/com/aranaira/arcanearchives/tileentities/RadiantResonatorTileEntity.java
@@ -2,5 +2,6 @@ package com.aranaira.arcanearchives.tileentities;
 
 public class RadiantResonatorTileEntity extends ImmanenceTileEntity 
 {
-	
+	public int TicksUntilCrystalGrowth;
+	public int BonusTicks; //How many bonus ticks the Resonator simulates based on supplied Immanence
 }
