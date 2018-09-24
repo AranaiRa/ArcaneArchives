55c53eed2ef5e50fd80c3224aef2c9ffbd631615
Lenyeto <patg.lenyeto@gmail.com>
2018-09-23 23:09:54 -0400

    Add base for raw quartz and misc model things.

---
+++ src/main/java/com/aranaira/arcanearchives/blocks/RawQuartz.java
@@ -0,0 +1,13 @@
+package com.aranaira.arcanearchives.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class RawQuartz extends Block {
+
+	public RawQuartz() {
+		super(Material.ROCK);
+		// TODO Auto-generated constructor stub
+	}
+
+}
