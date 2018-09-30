791c1b99a422908fa7bd9848e70115bd2bd714af
AranaiRa <aranaiseraph@gmail.com>
2018-09-29 23:42:35 -0400

    Fixed transparent textures on CMC

--- src/main/java/com/aranaira/arcanearchives/blocks/MatrixCrystalCore.java
+++ src/main/java/com/aranaira/arcanearchives/blocks/MatrixCrystalCore.java
@@ -20,6 +20,7 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.AxisAlignedBB;
@@ -170,5 +171,11 @@ public class MatrixCrystalCore extends BlockTemplate {
          }
 
          return entityIn.getHorizontalFacing().getOpposite();
+     }
+     
+     @Override
+     public BlockRenderLayer getBlockLayer()
+     {
+    	 return BlockRenderLayer.CUTOUT;
      }
 }
