387dc8818d93bfcef7e45a342f538cc4a36fcacc
AranaiRa <aranaiseraph@gmail.com>
2018-10-11 02:15:31 -0400

    Added crafting component items

--- src/main/java/com/aranaira/arcanearchives/init/ItemLibrary.java
+++ src/main/java/com/aranaira/arcanearchives/init/ItemLibrary.java
@@ -14,4 +14,11 @@ public class ItemLibrary {
 	public static final Item RAW_RADIANT_QUARTZ = new RawQuartzItem();
 	public static final Item CUT_RADIANT_QUARTZ = new CutQuartzItem();
 	public static final Item TOME_OF_REQUISITION = new TomeOfRequisitionItem();
+	
+	//CRAFTING COMPONENTS
+	public static final Item COMPONENT_CONTAINMENTFIELD = new ComponentContainmentFieldItem();
+	public static final Item COMPONENT_MATRIXBRACE = new ComponentMatrixBraceItem();
+	public static final Item COMPONENT_MATERIALINTERFACE = new ComponentMaterialInterfaceItem();
+	public static final Item COMPONENT_RADIANTDUST = new ComponentRadiantDustItem();
+	public static final Item COMPONENT_SCINTILLATINGINLAY = new ComponentScintillatingInlayItem();
 }
