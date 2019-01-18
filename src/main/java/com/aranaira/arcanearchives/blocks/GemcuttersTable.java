ce7b8c66eda8b93766f404b238b0b6f4b0babb72
Jon McManus <due@wxwhatever.com>
2019-01-18 16:35:50 +1000

    Remove some more block tile entity references.

--- src/main/java/com/aranaira/arcanearchives/blocks/GemcuttersTable.java
+++ src/main/java/com/aranaira/arcanearchives/blocks/GemcuttersTable.java
@@ -1,19 +1,12 @@
 package com.aranaira.arcanearchives.blocks;
 
 import java.util.List;
-import java.util.Random;
 
 import com.aranaira.arcanearchives.ArcaneArchives;
 import com.aranaira.arcanearchives.common.AAGuiHandler;
-import com.aranaira.arcanearchives.data.AAWorldSavedData;
 import com.aranaira.arcanearchives.tileentities.GemcuttersTableTileEntity;
-import com.aranaira.arcanearchives.tileentities.ImmanenceTileEntity;
-import com.aranaira.arcanearchives.tileentities.RadiantChestTileEntity;
-import com.aranaira.arcanearchives.tileentities.RadiantResonatorTileEntity;
-import com.aranaira.arcanearchives.util.NetworkHelper;
-import com.aranaira.arcanearchives.util.Placeable;
 
-import net.minecraft.block.Block;
+import com.aranaira.arcanearchives.util.Placeable;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.IProperty;
@@ -21,7 +14,6 @@ import net.minecraft.block.properties.PropertyDirection;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.util.ITooltipFlag;
-import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
@@ -31,22 +23,16 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
-import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
-import net.minecraftforge.client.model.b3d.B3DLoader;
-import net.minecraftforge.common.DimensionManager;
 import net.minecraftforge.common.property.ExtendedBlockState;
-import net.minecraftforge.common.property.IExtendedBlockState;
 import net.minecraftforge.common.property.IUnlistedProperty;
 import net.minecraftforge.common.property.Properties;
-import net.minecraftforge.items.IItemHandler;
 
 public class GemcuttersTable extends BlockTemplate implements ITileEntityProvider
 {
 	public static final PropertyDirection FACING = PropertyDirection.create("facing");
 	public static final String name = "gemcutters_table";
-	private TileEntity tileEntityInstance;
-	
+
 	public GemcuttersTable() 
 	{
 		super(name, Material.IRON);
@@ -65,19 +51,6 @@ public class GemcuttersTable extends BlockTemplate implements ITileEntityProvide
 		return true;
 	}
 
-	/*@Override
-	public boolean canPlaceBlockAt(World worldIn, BlockPos pos)
-	{
-		//NOTE : There may be a better way to get the player information for who is trying to place it.
-		//NOTE : If another player is closer to the block being placed it will go under that other player's network.
-		EntityPlayer EP = worldIn.getClosestPlayer(pos.getX(), pos.getY(), pos.getZ(), 100, false);
-		if (EP != null)
-			if (NetworkHelper.getArcaneArchivesNetwork(EP.getUniqueID()).CountBlocks(this) < PlaceLimit)
-				return true;
-		
-		return false;
-	}*/
-	
 	@Override
 	public boolean hasTileEntity(IBlockState state)
 	{
@@ -169,12 +142,6 @@ public class GemcuttersTable extends BlockTemplate implements ITileEntityProvide
  	@Override
  	public TileEntity createNewTileEntity(World worldIn, int meta) 
  	{
- 		return tileEntityInstance = new GemcuttersTableTileEntity();
- 	}
-
- 	@Override
- 	public TileEntity createTileEntity(World world, IBlockState state) 
- 	{
- 		return tileEntityInstance = new GemcuttersTableTileEntity();
+ 		return new GemcuttersTableTileEntity();
  	}
 }
