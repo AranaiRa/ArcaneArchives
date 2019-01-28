2f3077502f05164af9652aba54a6f481f2bfed56
Jon McManus <due@wxwhatever.com>
2019-01-28 15:19:34 +1000

    The setName/getName/name handling code from the ITE moved to the AATileEntity.

--- src/main/java/com/aranaira/arcanearchives/tileentities/AATileEntity.java
+++ src/main/java/com/aranaira/arcanearchives/tileentities/AATileEntity.java
@@ -1,24 +1,27 @@
 package com.aranaira.arcanearchives.tileentities;
 
-import com.aranaira.arcanearchives.blocks.BlockTemplate;
+import com.aranaira.arcanearchives.blocks.BlockDirectionalTemplate;
 import com.aranaira.arcanearchives.util.Placeable;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.world.World;
 
+import java.util.List;
+
 public class AATileEntity extends TileEntity
 {
+	public String name;
 	public Placeable.Size size;
-
-	public void setSize(Placeable.Size newSize)
-	{
-		this.size = newSize;
-	}
 
 	public Placeable.Size getSize()
 	{
 		return this.size;
+	}
+
+	public void setSize(Placeable.Size newSize)
+	{
+		this.size = newSize;
 	}
 
 	public boolean hasAccessors()
@@ -26,9 +29,25 @@ public class AATileEntity extends TileEntity
 		return this.size.hasAccessors();
 	}
 
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
 	public EnumFacing getFacing(World world)
 	{
 		IBlockState state = world.getBlockState(getPos());
-		return state.getValue(BlockTemplate.FACING);
+		if(state.getBlock() instanceof BlockDirectionalTemplate)
+		{
+			return state.getValue(BlockDirectionalTemplate.FACING);
+		} else
+		{
+			return null;
+		}
 	}
 }
