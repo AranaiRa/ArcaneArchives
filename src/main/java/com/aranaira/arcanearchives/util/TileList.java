a35a9c95f4718245e1a6ad5bffe33fec2c62c524
Jon McManus <due@wxwhatever.com>
2019-02-01 12:26:26 +1000

    Make TileList properly implement Iterable.

--- src/main/java/com/aranaira/arcanearchives/util/TileList.java
+++ src/main/java/com/aranaira/arcanearchives/util/TileList.java
@@ -7,10 +7,14 @@ import net.minecraft.world.World;
 
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
-public class TileList<T extends AATileEntity> implements Iterable<T>
+public class TileList<T extends AATileEntity> implements Iterable<T>, Collection<T>
 {
-	Collection<T> reference;
+	private Collection<T> reference;
 
 	public TileList(Collection<T> reference)
 	{
@@ -38,8 +42,110 @@ public class TileList<T extends AATileEntity> implements Iterable<T>
 	}
 
 	@Override
+	public int size()
+	{
+		return this.reference.size();
+	}
+
+	@Override
+	public boolean isEmpty()
+	{
+		return this.reference.isEmpty();
+	}
+
+	@Override
+	public boolean contains(Object o)
+	{
+		return this.reference.contains(o);
+	}
+
+	@Override
+	public Object[] toArray()
+	{
+		return this.reference.toArray();
+	}
+
+	@Override // TODO: Cheeck
+	public <T1> T1[] toArray(T1[] a)
+	{
+		return this.reference.toArray(a);
+	}
+
+	@Override
+	public boolean add(T t)
+	{
+		return this.reference.add(t);
+	}
+
+	@Override
+	public boolean remove(Object o)
+	{
+		return this.reference.remove(o);
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> c)
+	{
+		return this.reference.containsAll(c);
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends T> c)
+	{
+		return this.reference.addAll(c);
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> c)
+	{
+		return this.reference.removeAll(c);
+	}
+
+	@Override
+	public boolean removeIf(Predicate<? super T> filter)
+	{
+		return this.reference.removeIf(filter);
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> c)
+	{
+		return this.reference.retainAll(c);
+	}
+
+	@Override
+	public void clear()
+	{
+		this.reference.clear();
+	}
+
+	@Override
 	public Iterator<T> iterator()
 	{
 		return reference.iterator();
+	}
+
+	@Override
+	public void forEach(Consumer<? super T> action)
+	{
+		reference.forEach(action);
+	}
+
+	@Override
+	public Spliterator<T> spliterator()
+	{
+		return this.reference.spliterator();
+	}
+
+	@Override
+	public Stream<T> stream()
+	{
+		return this.reference.stream();
+	}
+
+	@Override
+	public Stream<T> parallelStream()
+	{
+		return this.reference.parallelStream();
 	}
 }
