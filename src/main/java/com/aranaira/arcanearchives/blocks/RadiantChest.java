804b64ed1ee327853fbec89c4fe917b1aea8aff0
Jon McManus <due@wxwhatever.com>
2019-02-07 13:21:23 +1000

    Talkarcabbage: Adjust the side player.openGui is fired on.
    Hopefully resolving issues with "ghost" inventories.

--- src/main/java/com/aranaira/arcanearchives/blocks/RadiantChest.java
+++ src/main/java/com/aranaira/arcanearchives/blocks/RadiantChest.java
@@ -73,6 +73,8 @@ public class RadiantChest extends BlockTemplate
 	{
 		RemoveChestLines(pos);
 
+		if (worldIn.isRemote) return true;
+
 		playerIn.openGui(ArcaneArchives.instance, AAGuiHandler.RADIANT_CHEST, worldIn, pos.getX(), pos.getY(), pos.getZ());
 
 		return true;
