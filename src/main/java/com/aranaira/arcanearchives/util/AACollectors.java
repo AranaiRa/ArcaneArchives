21d50a0de197da1968ff7ca28f4085bf49765312
Daomephsta <dtnilfisk@gmail.com>
2019-02-21 16:47:19 +1300

    Delete GenericCollector. It has no benefit over a call to Collector.of().

--- src/main/java/com/aranaira/arcanearchives/util/AACollectors.java
+++ src/main/java/com/aranaira/arcanearchives/util/AACollectors.java
@@ -1,10 +1,6 @@
 package com.aranaira.arcanearchives.util;
 
 import java.util.*;
-import java.util.function.BiConsumer;
-import java.util.function.BinaryOperator;
-import java.util.function.Function;
-import java.util.function.Supplier;
 import java.util.stream.Collector;
 
 public class AACollectors
@@ -18,67 +14,5 @@ public class AACollectors
 			left.addAll(right);
 			return left;
 		}, Collections::unmodifiableList);
-	}
-
-	// Don't look at this
-	@SuppressWarnings("unchecked")
-	private static <I, R> Function<I, R> castingIdentity()
-	{
-		return i -> (R) i;
-	}
-
-	public static class GenericCollector<T, A, R> implements Collector<T, A, R>
-	{
-		private final Supplier<A> supplier;
-		private final BiConsumer<A, T> accumulator;
-		private final BinaryOperator<A> combiner;
-		private final Function<A, R> finisher;
-		private final Set<Characteristics> characteristics;
-
-		GenericCollector(
-				Supplier<A> supplier, BiConsumer<A, T> accumulator, BinaryOperator<A> combiner, Function<A, R> finisher, Set<Characteristics> characteristics)
-		{
-			this.supplier = supplier;
-			this.accumulator = accumulator;
-			this.combiner = combiner;
-			this.finisher = finisher;
-			this.characteristics = characteristics;
-		}
-
-		GenericCollector(
-				Supplier<A> supplier, BiConsumer<A, T> accumulator, BinaryOperator<A> combiner, Set<Characteristics> characteristics)
-		{
-			this(supplier, accumulator, combiner, castingIdentity(), characteristics);
-		}
-
-		@Override
-		public BiConsumer<A, T> accumulator()
-		{
-			return accumulator;
-		}
-
-		@Override
-		public Supplier<A> supplier()
-		{
-			return supplier;
-		}
-
-		@Override
-		public BinaryOperator<A> combiner()
-		{
-			return combiner;
-		}
-
-		@Override
-		public Function<A, R> finisher()
-		{
-			return finisher;
-		}
-
-		@Override
-		public Set<Characteristics> characteristics()
-		{
-			return characteristics;
-		}
 	}
 }
