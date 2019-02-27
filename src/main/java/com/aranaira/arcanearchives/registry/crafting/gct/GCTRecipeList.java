8d680599ee45cadb0a92d1570c303a3ee9b42b96
Jon McManus <due@wxwhatever.com>
2019-02-27 13:01:30 +1000

    Begin work on converting GCT recipes to IRecipe base (shapeless).

---
+++ src/main/java/com/aranaira/arcanearchives/registry/crafting/gct/GCTRecipeList.java
@@ -0,0 +1,47 @@
+package com.aranaira.arcanearchives.registry.crafting.gct;
+
+import com.google.common.collect.ImmutableList;
+import net.minecraft.item.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GCTRecipeList
+{
+	private static final List<GCTRecipe> RECIPE_LIST = new ArrayList<>();
+	private static ImmutableList<GCTRecipe> IMMUTABLE_COPY = null;
+
+	public static List<GCTRecipe> getRecipeList() {
+		if (IMMUTABLE_COPY == null) IMMUTABLE_COPY = ImmutableList.copyOf(RECIPE_LIST);
+
+		return IMMUTABLE_COPY;
+	}
+
+	public static void addRecipe (GCTRecipe recipe) {
+		IMMUTABLE_COPY = null;
+		RECIPE_LIST.add(recipe);
+	}
+
+	public static void removeRecipe (GCTRecipe recipe) {
+		if (RECIPE_LIST.contains(recipe)) {
+			IMMUTABLE_COPY = null;
+			RECIPE_LIST.remove(recipe);
+		}
+	}
+
+	public static ItemStack getOutputByIndex (int index) {
+		if (index < 0 || index >= RECIPE_LIST.size()) return ItemStack.EMPTY;
+
+		return RECIPE_LIST.get(index).getRecipeOutput();
+	}
+
+	public static int indexOf (GCTRecipe recipe) {
+		if (recipe == null || !RECIPE_LIST.contains(recipe)) return -1;
+
+		return RECIPE_LIST.indexOf(recipe);
+	}
+
+	public static int getSize () {
+		return RECIPE_LIST.size();
+	}
+}
