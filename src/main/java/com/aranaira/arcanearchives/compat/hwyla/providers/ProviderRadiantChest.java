3029cce90e93961ef764145a71d9800926da0524
Jon McManus <due@wxwhatever.com>
2019-03-05 10:47:59 +1000

    Add translations for WAILA, at least.
    TOP apparently is server-side so I'll need to check if it takes
    TextComponents.

--- src/main/java/com/aranaira/arcanearchives/compat/hwyla/providers/ProviderRadiantChest.java
+++ src/main/java/com/aranaira/arcanearchives/compat/hwyla/providers/ProviderRadiantChest.java
@@ -4,6 +4,7 @@ import com.aranaira.arcanearchives.tileentities.RadiantChestTileEntity;
 import mcp.mobius.waila.api.IWailaConfigHandler;
 import mcp.mobius.waila.api.IWailaDataAccessor;
 import mcp.mobius.waila.api.IWailaDataProvider;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.TextFormatting;
 
@@ -18,8 +19,8 @@ public class ProviderRadiantChest implements IWailaDataProvider
 		if(te != null)
 		{
 			String chestName = te.getChestName();
-			if (!chestName.isEmpty())
-				tooltip.add(TextFormatting.GOLD + "Name: " + chestName);
+			if(!chestName.isEmpty())
+				tooltip.add(TextFormatting.GOLD + I18n.format("arcanearchives.data.tooltip.chest_name", chestName));
 		}
 
 		return tooltip;
--- src/main/java/com/aranaira/arcanearchives/compat/hwyla/providers/ProviderResonator.java
+++ src/main/java/com/aranaira/arcanearchives/compat/hwyla/providers/ProviderResonator.java
@@ -4,6 +4,7 @@ import com.aranaira.arcanearchives.tileentities.RadiantResonatorTileEntity;
 import mcp.mobius.waila.api.IWailaConfigHandler;
 import mcp.mobius.waila.api.IWailaDataAccessor;
 import mcp.mobius.waila.api.IWailaDataProvider;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.TextFormatting;
 
@@ -16,8 +17,8 @@ public class ProviderResonator implements IWailaDataProvider
 	{
 		RadiantResonatorTileEntity te = (RadiantResonatorTileEntity) accessor.getTileEntity();
 		if (te != null) {
-			tooltip.add(String.format(TextFormatting.GOLD + "Progress: %d%%", te.getPercentageComplete()));
-			tooltip.add(te.canTick() ? TextFormatting.GREEN + "Resonating" : TextFormatting.RED + "Offline");
+			tooltip.add(TextFormatting.GOLD + I18n.format("arcanearchives.data.tooltip.resonator_progress", te.getPercentageComplete()));
+			tooltip.add(te.canTick() ? TextFormatting.GREEN + I18n.format("arcanearchives.data.tooltip.resonator_satus.online"): TextFormatting.RED + I18n.format("arcanearchives.data.tooltip.resonator_status.offline"));
 		}
 
 		return tooltip;
