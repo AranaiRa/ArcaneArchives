2d37faa6e112ca3ec97deba1e2ccf761cfbe3855
Jon McManus <due@wxwhatever.com>
2019-03-15 19:08:53 +1000

    Implement sneak-keybind to clear manifest search lines.

--- src/main/java/com/aranaira/arcanearchives/client/Keybinds.java
+++ src/main/java/com/aranaira/arcanearchives/client/Keybinds.java
@@ -1,6 +1,8 @@
 package com.aranaira.arcanearchives.client;
 
 import com.aranaira.arcanearchives.ArcaneArchives;
+import com.aranaira.arcanearchives.events.LineHandler;
+import com.aranaira.arcanearchives.init.ItemRegistry;
 import com.aranaira.arcanearchives.items.ManifestItem;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.KeyBinding;
@@ -40,7 +42,7 @@ public class Keybinds
 			for(int i = 0; i < 36; i++)
 			{
 				ItemStack item = mc.player.inventory.getStackInSlot(i);
-				if(item.getItem() instanceof ManifestItem)
+				if(item.getItem() == ItemRegistry.MANIFEST)
 				{
 					foundManifest = true;
 					break;
@@ -49,7 +51,13 @@ public class Keybinds
 
 			if(foundManifest)
 			{
-				ManifestItem.openManifest(mc.player.world, mc.player);
+				if(mc.player.isSneaking())
+				{
+					LineHandler.clearChests(mc.player.dimension);
+				} else
+				{
+					ManifestItem.openManifest(mc.player.world, mc.player);
+				}
 			} else
 			{
 				mc.player.sendMessage(new TextComponentTranslation("arcanearchives.gui.missing_manifest").setStyle(new Style().setColor(TextFormatting.YELLOW)));
