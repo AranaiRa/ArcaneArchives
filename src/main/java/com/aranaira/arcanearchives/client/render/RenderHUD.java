e8e335fb1ba15637fd9b9154adf33a138aa29b6d
Jon McManus <due@wxwhatever.com>
2019-04-06 13:38:08 +1000

    Change logic for and insert some debug statements for disappearing HUD.

--- src/main/java/com/aranaira/arcanearchives/client/render/RenderHUD.java
+++ src/main/java/com/aranaira/arcanearchives/client/render/RenderHUD.java
@@ -27,11 +27,20 @@ public class RenderHUD
 	private RenderHUD () {
 	}
 
+	// TODO: DEBUGGING
+	private static int lastDrew = 0;
+
 	@SideOnly(Side.CLIENT)
 	@SubscribeEvent(priority= EventPriority.HIGHEST)
 	public static void onDrawScreenPost (RenderGameOverlayEvent.Post event) {
 		Minecraft mc = Minecraft.getMinecraft();
+
 		if (event.getType() == RenderGameOverlayEvent.ElementType.ALL) {
+			if(mc.player.ticksExisted - lastDrew < 0)
+			{
+				lastDrew = Math.max(0, mc.player.ticksExisted - 60);
+			}
+
 			RayTraceResult pos = mc.objectMouseOver;
 
 			if (pos != null && pos.typeOfHit == RayTraceResult.Type.BLOCK) {
@@ -47,7 +56,8 @@ public class RenderHUD
 						TroveItemHandler handler = te.getInventory();
 
 						ItemStack item = handler.getItemCurrent();
-						if (!item.isEmpty()) {
+						if(!handler.isEmpty())
+						{
 							mc.getRenderItem().renderItemIntoGUI(item, x - 40, y);
 							String s = "x " + NumberUtil.format(handler.getCount());
 							mc.fontRenderer.drawStringWithShadow(item.getDisplayName(), (float)(x - 19 -  mc.fontRenderer.getStringWidth(item.getDisplayName()) / 2), (float)(y - 11), 16777215);
@@ -56,25 +66,35 @@ public class RenderHUD
 								if (handler.getUpgrades() == 1)
 								{
 									s = I18n.format("arcanearchives.data.gui.radiant_trove.upgrade", handler.getUpgrades());
-								}
-								else {
+								} else {
 									s = I18n.format("arcanearchives.data.gui.radiant_trove.upgrades", handler.getUpgrades());
 								}
 								mc.fontRenderer.drawStringWithShadow(TextFormatting.GOLD + s, (float)(x - 19 - mc.fontRenderer.getStringWidth(s) / 2), (float) (y + 20), 16777215);
 							}
 						} else if (handler.getUpgrades() != 0) {
-							if (handler.getUpgrades() != 0)
+							String s;
+							if(handler.getUpgrades() == 1)
 							{
-								String s;
-								if(handler.getUpgrades() == 1)
-								{
-									s = I18n.format("arcanearchives.data.gui.radiant_trove.upgrade", handler.getUpgrades());
-								} else
-								{
-									s = I18n.format("arcanearchives.data.gui.radiant_trove.upgrades", handler.getUpgrades());
-								}
-								mc.fontRenderer.drawStringWithShadow(TextFormatting.GOLD + s, (float) (x - 19 - mc.fontRenderer.getStringWidth(s) / 2), (float) (y), 16777215);
+								s = I18n.format("arcanearchives.data.gui.radiant_trove.upgrade", handler.getUpgrades());
+							} else
+							{
+								s = I18n.format("arcanearchives.data.gui.radiant_trove.upgrades", handler.getUpgrades());
 							}
+							mc.fontRenderer.drawStringWithShadow(TextFormatting.GOLD + s, (float) (x - 19 - mc.fontRenderer.getStringWidth(s) / 2), (float) (y), 16777215);
+						} else
+						{
+							if(mc.player.ticksExisted - lastDrew >= 40)
+							{
+								ArcaneArchives.logger.error("DEBUG: Not drawing a trove as it appears to be un-upgraded and empty.");
+								lastDrew = mc.player.ticksExisted;
+							}
+						}
+					} else
+					{
+						if(mc.player.ticksExisted - lastDrew >= 40)
+						{
+							ArcaneArchives.logger.error("DEBUG: Can't draw trove as there isn't a tile entity to access!");
+							lastDrew = mc.player.ticksExisted;
 						}
 					}
 				}
