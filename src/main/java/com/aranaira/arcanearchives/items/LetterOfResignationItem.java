3face810e0032e436c5e6029d2defb6a41c87fe2
AranaiRa <aranaiseraph@gmail.com>
2019-04-30 01:43:29 -0400

    Added Letter of Invitation and Letter of Resignation (hopefully!)

---
+++ src/main/java/com/aranaira/arcanearchives/items/LetterOfResignationItem.java
@@ -0,0 +1,12 @@
+package com.aranaira.arcanearchives.items;
+
+import com.aranaira.arcanearchives.items.templates.ItemTemplate;
+
+public class LetterOfResignationItem extends ItemTemplate
+{
+	public static final String NAME = "item_letterofresignation";
+
+	public LetterOfResignationItem() {
+		super(NAME);
+	}
+}
