9d11e4a97dbb6434cf48c2a211e57fe6c35877fc
AranaiRa <aranaiseraph@gmail.com>
2019-05-12 17:43:51 -0400

    Added brazier of hoarding model

---
+++ src/main/java/com/aranaira/arcanearchives/blocks/BrazierOfHoarding.java
@@ -0,0 +1,43 @@
+package com.aranaira.arcanearchives.blocks;
+
+import com.aranaira.arcanearchives.blocks.templates.BlockTemplate;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.List;
+
+public class BrazierOfHoarding extends BlockTemplate
+{
+
+	public static final String name = "brazier_of_hoarding";
+
+	public BrazierOfHoarding() {
+		super(name, Material.GLASS);
+		setLightLevel(16 / 16f);
+	}
+
+	@Override
+	public boolean hasOBJModel() {
+		return true;
+	}
+
+	@Override
+	@SuppressWarnings("deprecation")
+	public boolean isOpaqueCube(IBlockState state) {
+		return false;
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack stack, World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+		tooltip.add(TextFormatting.RED + "" + TextFormatting.BOLD + I18n.format("arcanearchives.tooltip.notimplemented1"));
+		tooltip.add(TextFormatting.RED + "" + TextFormatting.ITALIC + I18n.format("arcanearchives.tooltip.notimplemented2"));
+	}
+}
