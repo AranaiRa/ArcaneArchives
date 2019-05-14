a6176b1bdd10049fe4d6327f610cdee88c9c54bc
AranaiRa <aranaiseraph@gmail.com>
2019-05-14 02:50:09 -0400

    Added tooltip to Raw Quartz Clusters and changed its name.

--- src/main/java/com/aranaira/arcanearchives/blocks/RawQuartz.java
+++ src/main/java/com/aranaira/arcanearchives/blocks/RawQuartz.java
@@ -4,13 +4,19 @@ import com.aranaira.arcanearchives.blocks.templates.BlockTemplate;
 import com.aranaira.arcanearchives.init.ItemRegistry;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class RawQuartz extends BlockTemplate {
 
@@ -23,6 +29,11 @@ public class RawQuartz extends BlockTemplate {
 		setHardness(1.4f);
 		//setDefaultState(this.blockState.getBaseState().withProperty(DIRECTION,  EnumFacing.NORTH));
 		setHarvestLevel("pickaxe", 0);
+	}
+
+	@Override
+	public void addInformation (ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+		tooltip.add(TextFormatting.GOLD + I18n.format("arcanearchives.tooltip.item.rawquartz"));
 	}
 
 	@Override
