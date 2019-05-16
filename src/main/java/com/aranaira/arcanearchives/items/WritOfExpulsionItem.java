c02e9a7a8eb99cda45260a17e63927e76b9dd069
AranaiRa <aranaiseraph@gmail.com>
2019-05-16 10:08:03 -0400

    Added Writ of Expulsion

---
+++ src/main/java/com/aranaira/arcanearchives/items/WritOfExpulsionItem.java
@@ -0,0 +1,24 @@
+package com.aranaira.arcanearchives.items;
+
+import com.aranaira.arcanearchives.items.templates.ItemTemplate;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
+public class WritOfExpulsionItem extends ItemTemplate {
+	public static final String NAME = "item_writofexpulsion";
+
+	public WritOfExpulsionItem() {
+		super(NAME);
+	}
+
+	@Override
+	public void addInformation (ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+		tooltip.add(TextFormatting.GOLD + I18n.format("arcanearchives.tooltip.item.writofexpulsion"));
+	}
+}
