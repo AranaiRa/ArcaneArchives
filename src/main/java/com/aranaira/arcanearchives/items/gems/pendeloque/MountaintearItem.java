26d11fa21e5df6e237fa4b5b38993a518a4a5c42
AranaiRa <aranaiseraph@gmail.com>
2019-05-31 00:10:50 -0400

    Mountaintear doesn't need all that commented out code, oops

--- src/main/java/com/aranaira/arcanearchives/items/gems/pendeloque/MountaintearItem.java
+++ src/main/java/com/aranaira/arcanearchives/items/gems/pendeloque/MountaintearItem.java
@@ -65,27 +65,9 @@ public class MountaintearItem extends ArcaneGemItem {
                 PacketDoParticles packet = new PacketDoParticles(cut, color, start, end);
                 NetworkRegistry.TargetPoint tp = new NetworkRegistry.TargetPoint(player.dimension, start.x, start.y, start.z, 160);
                 NetworkHandler.CHANNEL.sendToAllAround(packet, tp);
-                //ParticleGenerator.makeDefaultLine(world, start, end, 40, 2.0);
-                //ParticleGenerator.makeDefaultBurst(world, end, 36, 1,0.6, 0.01, 0.03);
             }
         } else {
-            /*Vec3d start = new Vec3d(player.posX, player.posY+1, player.posZ);
-            Vec3d dir = player.getLookVec();
-            Vec3d rayTarget = new Vec3d(start.x + dir.x * 40, start.y + dir.y * 40, start.z + dir.z * 40);
-
-            RayTraceResult ray = world.rayTraceBlocks(start, rayTarget, false, true, false);
-
-            if(ray != null) {
-                BlockPos pos = ray.getBlockPos();
-                EnumFacing facing = ray.sideHit;
-
-                Vec3d end = new Vec3d(pos.offset(facing).getX(), pos.offset(facing).getY(), pos.offset(facing).getZ());
-
-                PacketDoParticles packet = new PacketDoParticles(start, end);
-                NetworkHandler.CHANNEL.sendToServer(packet);
-                //ParticleGenerator.makeDefaultLine(world, start, end, 40, 2.0);
-                //ParticleGenerator.makeDefaultBurst(world, end, 36, 1,0.6, 0.01, 0.03);
-            }*/
+            //TODO: actually place lava OR SOMETHING
         }
         return new ActionResult<>(EnumActionResult.SUCCESS, player.getHeldItem(hand));
     }
