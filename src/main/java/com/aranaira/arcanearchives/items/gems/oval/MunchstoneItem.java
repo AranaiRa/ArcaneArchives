ea38de18558aa3ba3eda0f1797b7ce6d3dcac213
AranaiRa <aranaiseraph@gmail.com>
2019-06-08 02:45:05 -0400

    Added nonfunctional Munchstone item

---
+++ src/main/java/com/aranaira/arcanearchives/items/gems/oval/MunchstoneItem.java
@@ -0,0 +1,51 @@
+package com.aranaira.arcanearchives.items.gems.oval;
+
+import com.aranaira.arcanearchives.items.gems.ArcaneGemItem;
+import com.aranaira.arcanearchives.network.NetworkHandler;
+import com.aranaira.arcanearchives.network.PacketArcaneGem;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.item.EnumRarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.common.network.NetworkRegistry;
+
+import java.util.List;
+
+public class MunchstoneItem extends ArcaneGemItem {
+    public static final String NAME = "item_munchstone";
+
+    public MunchstoneItem() { super(NAME, GemCut.OVAL, GemColor.BLACK, 25, 100); }
+
+    @Override
+    public void addInformation (ItemStack stack, World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+        tooltip.add(I18n.format("arcanearchives.tooltip.gemcharge") + ": " +getTooltipData(stack));
+        tooltip.add(TextFormatting.GOLD + I18n.format("arcanearchives.tooltip.item.munchstone"));
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public EnumRarity getRarity (ItemStack stack) {
+        return EnumRarity.UNCOMMON;
+    }
+
+    @Override
+    public boolean doesSneakBypassUse (ItemStack stack, IBlockAccess world, BlockPos pos, EntityPlayer player) {
+        return false;
+    }
+
+    @Override
+    public ActionResult<ItemStack> onItemRightClick (World world, EntityPlayer player, EnumHand hand) {
+
+        return new ActionResult<>(EnumActionResult.SUCCESS, player.getHeldItem(hand));
+    }
+}
