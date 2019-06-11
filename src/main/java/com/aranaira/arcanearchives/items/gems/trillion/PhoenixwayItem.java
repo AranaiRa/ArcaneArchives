0394822d18d423af0720e3283dca322aceebee7f
AranaiRa <aranaiseraph@gmail.com>
2019-06-11 02:18:37 -0400

    Phoenixway now raycasts from the proper height

--- src/main/java/com/aranaira/arcanearchives/items/gems/trillion/PhoenixwayItem.java
+++ src/main/java/com/aranaira/arcanearchives/items/gems/trillion/PhoenixwayItem.java
@@ -49,7 +49,7 @@ public class PhoenixwayItem extends ArcaneGemItem {
     public ActionResult<ItemStack> onItemRightClick (World world, EntityPlayer player, EnumHand hand) {
         if(!world.isRemote) {
             if (GemUtil.getCharge(player.getHeldItemMainhand()) > 0) {
-                Vec3d start = new Vec3d(player.posX, player.posY + 1, player.posZ);
+                Vec3d start = new Vec3d(player.posX, player.posY + player.height, player.posZ);
                 Vec3d dir = player.getLookVec();
                 Vec3d rayTarget = new Vec3d(start.x + dir.x * 40, start.y + dir.y * 40, start.z + dir.z * 40);
 
