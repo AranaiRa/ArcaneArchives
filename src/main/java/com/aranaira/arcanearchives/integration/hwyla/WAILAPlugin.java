16cdec2673f071f6234b88334b945a4fc9b41f3c
Jon McManus <due@wxwhatever.com>
2019-06-11 20:42:00 +1000

    WAILA/TOP support for troves.
    TOP automatically supports tanks, might need to handle things
    differently for WAILA?

--- src/main/java/com/aranaira/arcanearchives/integration/hwyla/WAILAPlugin.java
+++ src/main/java/com/aranaira/arcanearchives/integration/hwyla/WAILAPlugin.java
@@ -1,9 +1,11 @@
 package com.aranaira.arcanearchives.integration.hwyla;
 
 import com.aranaira.arcanearchives.integration.hwyla.providers.ProviderRadiantChest;
+import com.aranaira.arcanearchives.integration.hwyla.providers.ProviderRadiantTrove;
 import com.aranaira.arcanearchives.integration.hwyla.providers.ProviderResonator;
 import com.aranaira.arcanearchives.tileentities.RadiantChestTileEntity;
 import com.aranaira.arcanearchives.tileentities.RadiantResonatorTileEntity;
+import com.aranaira.arcanearchives.tileentities.RadiantTroveTileEntity;
 import mcp.mobius.waila.api.IWailaPlugin;
 import mcp.mobius.waila.api.IWailaRegistrar;
 import mcp.mobius.waila.api.WailaPlugin;
@@ -19,5 +21,6 @@ public class WAILAPlugin implements IWailaPlugin {
 	public void register (IWailaRegistrar registrar) {
 		registrar.registerBodyProvider(new ProviderResonator(), RadiantResonatorTileEntity.class);
 		registrar.registerBodyProvider(new ProviderRadiantChest(), RadiantChestTileEntity.class);
+		registrar.registerBodyProvider(new ProviderRadiantTrove(), RadiantTroveTileEntity.class);
 	}
 }
---
+++ src/main/java/com/aranaira/arcanearchives/integration/hwyla/providers/ProviderRadiantTrove.java
@@ -0,0 +1,30 @@
+package com.aranaira.arcanearchives.integration.hwyla.providers;
+
+import com.aranaira.arcanearchives.inventory.handlers.TroveItemHandler;
+import com.aranaira.arcanearchives.tileentities.RadiantChestTileEntity;
+import com.aranaira.arcanearchives.tileentities.RadiantTroveTileEntity;
+import mcp.mobius.waila.api.IWailaConfigHandler;
+import mcp.mobius.waila.api.IWailaDataAccessor;
+import mcp.mobius.waila.api.IWailaDataProvider;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.TextFormatting;
+
+import java.util.List;
+
+public class ProviderRadiantTrove implements IWailaDataProvider {
+	@Override
+	public List<String> getWailaBody (ItemStack itemStack, List<String> tooltip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
+		RadiantTroveTileEntity te = (RadiantTroveTileEntity) accessor.getTileEntity();
+		if (te != null) {
+			TroveItemHandler handler = te.getInventory();
+			if (handler.getCount() != 0) {
+				tooltip.add(TextFormatting.GOLD + I18n.format("arcanearchives.data.tooltip.trove_count_waila", handler.getCount(), handler.getItem().getDisplayName()));
+			} else {
+				tooltip.add(TextFormatting.GOLD + I18n.format("arcanearchives.data.tooltip.empty"));
+			}
+		}
+
+		return tooltip;
+	}
+}
