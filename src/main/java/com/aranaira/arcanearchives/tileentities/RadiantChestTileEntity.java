367b84fc1c797cacf324708aa7daef51a894c20a
Jon McManus <due@wxwhatever.com>
2019-06-11 21:54:47 +1000

    A start at a hash-storage for item counts.

--- src/main/java/com/aranaira/arcanearchives/tileentities/RadiantChestTileEntity.java
+++ src/main/java/com/aranaira/arcanearchives/tileentities/RadiantChestTileEntity.java
@@ -3,6 +3,8 @@ package com.aranaira.arcanearchives.tileentities;
 import com.aranaira.arcanearchives.ArcaneArchives;
 import com.aranaira.arcanearchives.network.NetworkHandler;
 import com.aranaira.arcanearchives.network.PacketRadiantChest;
+import it.unimi.dsi.fastutil.ints.Int2IntOpenHashMap;
+import net.minecraft.client.util.RecipeItemHelper;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.NetworkManager;
@@ -131,6 +133,75 @@ public class RadiantChestTileEntity extends ManifestTileEntity {
 		public static final String CHEST_NAME = "chestName";
 
 		private Tags () {
+		}
+	}
+
+	private static class RadiantChestHandler extends ItemStackHandler {
+		public int emptySlots = 0;
+		public Int2IntOpenHashMap itemReference = new Int2IntOpenHashMap();
+
+		public RadiantChestHandler (int size) {
+			super(size);
+			itemReference.defaultReturnValue(-1);
+		}
+
+		@Override
+		public void setStackInSlot (int slot, @Nonnull ItemStack stack) {
+			ItemStack curStack = getStackInSlot(slot);
+			int current = RecipeItemHelper.pack(getStackInSlot(slot));
+			int count = itemReference.get(current);
+			if (count != -1) {
+				count -= curStack.getCount();
+				itemReference.put(current, count);
+			}
+			if (stack.isEmpty()) {
+				emptySlots++;
+			}
+			super.setStackInSlot(slot, stack);
+		}
+
+		@Nonnull
+		@Override
+		public ItemStack insertItem (int slot, @Nonnull ItemStack stack, boolean simulate) {
+			if (!simulate) {
+				ItemStack inSlot = getStackInSlot(slot);
+				if (inSlot.isEmpty()) {
+					emptySlots--;
+				}
+
+				ItemStack test = super.insertItem(slot, stack, true);
+				int current = RecipeItemHelper.pack(stack);
+				int count = stack.getCount();
+				if (!test.isEmpty()) {
+					count -= test.getCount();
+				}
+				int curCount = itemReference.get(current);
+				if (curCount != -1) {
+					count += curCount;
+				}
+				itemReference.put(current, count);
+			}
+			return super.insertItem(slot, stack, simulate);
+		}
+
+		@Nonnull
+		@Override
+		public ItemStack extractItem (int slot, int amount, boolean simulate) {
+			if (!simulate) {
+				ItemStack test = super.extractItem(slot, amount, true);
+				int current = RecipeItemHelper.pack(test);
+				int curCount = itemReference.get(current);
+				if (curCount != -1) {
+					itemReference.put(current, curCount - test.getCount());
+				}
+			}
+			ItemStack result = super.extractItem(slot, amount, simulate);
+			if (!simulate) {
+				if (getStackInSlot(slot).isEmpty()) {
+					emptySlots++;
+				}
+			}
+			return result;
 		}
 	}
 }
