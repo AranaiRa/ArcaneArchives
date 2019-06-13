88973e84ca872000e0ec73a344c2a0829d7fbde5
Jon McManus <due@wxwhatever.com>
2019-06-14 09:45:25 +1000

    Literally fuck you IntelliJ.

--- src/main/java/com/aranaira/arcanearchives/proxy/ClientProxy.java
+++ src/main/java/com/aranaira/arcanearchives/proxy/ClientProxy.java
@@ -76,7 +76,7 @@ public class ClientProxy extends CommonProxy {
 
 	@Override
 	public void loadComplete (FMLLoadCompleteEvent event) {
-		if (Loader.isModLoaded("guidebook")) {
+		if (Loader.isModLoaded("gbook")) {
 			GBookInit.init();
 		}
 	}
