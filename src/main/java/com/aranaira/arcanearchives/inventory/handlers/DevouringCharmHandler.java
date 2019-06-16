d9b341a2c377660e7704ca6d2ea5d206d5602c6d
AranaiRa <aranaiseraph@gmail.com>
2019-06-16 02:57:24 -0400

    Devouring Charm now properly voids fluids

--- src/main/java/com/aranaira/arcanearchives/inventory/handlers/DevouringCharmHandler.java
+++ src/main/java/com/aranaira/arcanearchives/inventory/handlers/DevouringCharmHandler.java
@@ -4,6 +4,8 @@ import com.aranaira.arcanearchives.items.gems.ArcaneGemItem;
 import com.aranaira.arcanearchives.items.gems.GemUtil;
 import com.aranaira.arcanearchives.items.gems.pendeloque.ParchtearItem;
 import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBucket;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.common.util.INBTSerializable;
@@ -24,6 +26,9 @@ public class DevouringCharmHandler implements INBTSerializable<NBTTagCompound> {
 					return true;
 				}
 				boolean validItem = false;
+				validItem |= stack.getItem() == Items.WATER_BUCKET;
+				validItem |= stack.getItem() == Items.LAVA_BUCKET;
+				validItem |= stack.getItem() == Items.MILK_BUCKET;
 				validItem |= stack.getItem() instanceof UniversalBucket;
 				return validItem;
 			}
@@ -33,6 +38,17 @@ public class DevouringCharmHandler implements INBTSerializable<NBTTagCompound> {
 
 		@Override
 		protected void onContentsChanged (int slot) {
+			Item item = getStackInSlot(slot).getItem();
+			boolean shouldSwap = false;
+			shouldSwap |= item == Items.WATER_BUCKET;
+			shouldSwap |= item == Items.LAVA_BUCKET;
+			shouldSwap |= item == Items.MILK_BUCKET;
+			shouldSwap |= item instanceof UniversalBucket;
+
+			if(shouldSwap) {
+				setStackInSlot(slot, new ItemStack(Items.BUCKET));
+			}
+			//TODO: Dump fluid contents of things with a fluid capability
 			super.onContentsChanged(slot);
 		}
 	};
