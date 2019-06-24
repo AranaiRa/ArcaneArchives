a5ccb63ba69fb19f5766a4431d3209ef309c3936
AranaiRa <aranaiseraph@gmail.com>
2019-06-24 03:26:33 -0400

    Added descriptive tooltip to the Brazier

--- src/main/java/com/aranaira/arcanearchives/blocks/Brazier.java
+++ src/main/java/com/aranaira/arcanearchives/blocks/Brazier.java
@@ -59,8 +59,7 @@ public class Brazier extends BlockTemplate {
 	@Override
 	@SideOnly(Side.CLIENT)
 	public void addInformation (ItemStack stack, World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
-		tooltip.add(TextFormatting.RED + "" + TextFormatting.BOLD + I18n.format("arcanearchives.tooltip.notimplemented1"));
-		tooltip.add(TextFormatting.RED + "" + TextFormatting.ITALIC + I18n.format("arcanearchives.tooltip.notimplemented2"));
+		tooltip.add(TextFormatting.GOLD + I18n.format("arcanearchives.tooltip.device.brazier"));
 	}
 
 	@Override
