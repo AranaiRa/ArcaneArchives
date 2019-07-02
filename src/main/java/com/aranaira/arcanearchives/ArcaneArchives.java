f0edfaaec2ebafb3e38f5d6f2248434763958411
AranaiRa <aranaiseraph@gmail.com>
2019-07-02 02:58:04 -0400

    Added default Roots/MysticalWorld config to the Munchstone

--- src/main/java/com/aranaira/arcanearchives/ArcaneArchives.java
+++ src/main/java/com/aranaira/arcanearchives/ArcaneArchives.java
@@ -8,7 +8,7 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.*;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ArcaneArchives.MODID, name = ArcaneArchives.NAME, version = ArcaneArchives.VERSION, dependencies = "required-after:gbook_snapshot;after:baubles;required-before:mysticallib;after:thaumcraft;after:betternether")
+@Mod(modid = ArcaneArchives.MODID, name = ArcaneArchives.NAME, version = ArcaneArchives.VERSION, dependencies = "required-after:gbook_snapshot;after:baubles;required-before:mysticallib;after:thaumcraft")
 public class ArcaneArchives {
 	public static final String MODID = "arcanearchives";
 	public static final String NAME = "Arcane Archives";
