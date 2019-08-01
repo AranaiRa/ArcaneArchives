1f35561f8bf763837e5a4cfb9e03efb71f17b3c5
AranaiRa <aranaiseraph@gmail.com>
2019-07-31 20:20:00 -0400

    Monitoring Crystal pops off of Radiant storage

--- src/main/java/com/aranaira/arcanearchives/tileentities/MonitoringCrystalTileEntity.java
+++ src/main/java/com/aranaira/arcanearchives/tileentities/MonitoringCrystalTileEntity.java
@@ -1,12 +1,20 @@
 package com.aranaira.arcanearchives.tileentities;
 
+import com.aranaira.arcanearchives.ArcaneArchives;
 import com.aranaira.arcanearchives.blocks.MonitoringCrystal;
+import com.aranaira.arcanearchives.data.ServerNetwork;
 import com.aranaira.arcanearchives.tileentities.interfaces.IManifestTileEntity;
+import com.aranaira.arcanearchives.util.WorldUtil;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.Style;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.wrapper.InvWrapper;
@@ -70,5 +78,21 @@ public class MonitoringCrystalTileEntity extends ImmanenceTileEntity implements 
 		}
 
 		return target;
+	}
+
+	@Override
+	public void firstJoinedNetwork(ServerNetwork network) {
+		if(!getWorld().isRemote) {
+			BlockPos target = getTarget();
+			ImmanenceTileEntity ite = WorldUtil.getTileEntity(ImmanenceTileEntity.class, world, target);
+			if(ite == null)
+				super.firstJoinedNetwork(network);
+			else {
+				EntityPlayer player = getServerNetwork().getPlayer();
+				ITextComponent message = new TextComponentTranslation("arcanearchives.message.invalidMonitoringCrystalTarget" ).setStyle(new Style().setColor(TextFormatting.GOLD).setBold(true));
+				player.sendStatusMessage(message, true);
+				breakBlock();
+			}
+		}
 	}
 }
