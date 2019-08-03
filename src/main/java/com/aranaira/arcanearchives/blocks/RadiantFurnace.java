623376141eabe1d3e8dde232e9ca15d5d290fef9
AranaiRa <aranaiseraph@gmail.com>
2019-08-03 05:23:24 -0400

    Added teste model for the Radiant Furnace

--- src/main/java/com/aranaira/arcanearchives/blocks/RadiantFurnace.java
+++ src/main/java/com/aranaira/arcanearchives/blocks/RadiantFurnace.java
@@ -1,10 +1,14 @@
 package com.aranaira.arcanearchives.blocks;
 
+import com.aranaira.arcanearchives.blocks.templates.BlockDirectionalTemplate;
 import com.aranaira.arcanearchives.blocks.templates.BlockTemplate;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
@@ -24,9 +28,26 @@ public class RadiantFurnace extends BlockTemplate {
 	}
 
 	@Override
+	public boolean hasOBJModel () {
+		return true;
+	}
+
+	@Override
 	@SideOnly(Side.CLIENT)
 	public void addInformation (ItemStack stack, World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
 		tooltip.add(TextFormatting.RED + "" + TextFormatting.BOLD + I18n.format("arcanearchives.tooltip.notimplemented1"));
 		tooltip.add(TextFormatting.RED + "" + TextFormatting.ITALIC + I18n.format("arcanearchives.tooltip.notimplemented2"));
+	}
+
+	@Override
+	@SuppressWarnings("deprecation")
+	public boolean isFullCube (IBlockState state) {
+		return false;
+	}
+
+	@Override
+	@SuppressWarnings("deprecation")
+	public boolean isOpaqueCube (IBlockState state) {
+		return false;
 	}
 }
