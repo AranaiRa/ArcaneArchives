9c5c436d3fb34601db62ad0f4c56c3c027b06b9a
AranaiRa <aranaiseraph@gmail.com>
2019-08-13 01:50:03 -0400

    Added Verdant Censer model (untextured)

--- src/main/java/com/aranaira/arcanearchives/blocks/VerdantCenser.java
+++ src/main/java/com/aranaira/arcanearchives/blocks/VerdantCenser.java
@@ -2,9 +2,11 @@ package com.aranaira.arcanearchives.blocks;
 
 import com.aranaira.arcanearchives.blocks.templates.BlockTemplate;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
@@ -12,11 +14,11 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 import java.util.List;
 
-public class DruidicCenser extends BlockTemplate {
+public class VerdantCenser extends BlockTemplate {
 
-	public static final String name = "druidic_censer";
+	public static final String name = "verdant_censer";
 
-	public DruidicCenser() {
+	public VerdantCenser() {
 		super(name, Material.ROCK);
 		setLightLevel(16 / 16f);
 		setHardness(1.7f);
@@ -28,5 +30,22 @@ public class DruidicCenser extends BlockTemplate {
 	public void addInformation (ItemStack stack, World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
 		tooltip.add(TextFormatting.RED + "" + TextFormatting.BOLD + I18n.format("arcanearchives.tooltip.notimplemented1"));
 		tooltip.add(TextFormatting.RED + "" + TextFormatting.ITALIC + I18n.format("arcanearchives.tooltip.notimplemented2"));
+	}
+
+	@Override
+	public BlockRenderLayer getRenderLayer () {
+		return BlockRenderLayer.CUTOUT;
+	}
+
+	@Override
+	@SuppressWarnings("deprecation")
+	public boolean isFullCube (IBlockState state) {
+		return false;
+	}
+
+	@Override
+	@SuppressWarnings("deprecation")
+	public boolean isOpaqueCube (IBlockState state) {
+		return false;
 	}
 }
