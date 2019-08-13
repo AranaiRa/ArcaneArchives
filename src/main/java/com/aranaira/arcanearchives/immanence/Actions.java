fcd37900046efeaed28701d5b000befebe89e095
Jon McManus <due@wxwhatever.com>
2019-08-13 11:31:30 +1000

    More skeletal work on the immanence actions.

--- src/main/java/com/aranaira/arcanearchives/immanence/Actions.java
+++ src/main/java/com/aranaira/arcanearchives/immanence/Actions.java
@@ -1,19 +1,42 @@
 package com.aranaira.arcanearchives.immanence;
 
+import java.util.UUID;
+
 public class Actions {
-	public interface IImmanenceAction {
+	public static abstract class ImmanenceAction {
+		protected UUID uuid;
 
+		public ImmanenceAction (UUID uuid) {
+			this.uuid = uuid;
+		}
+
+		public UUID getUuid () {
+			return uuid;
+		}
 	}
 
-	public static class ReturnAction implements IImmanenceAction {
-
+	public static class ReturnAction extends ImmanenceAction {
+		public ReturnAction (UUID uuid) {
+			super(uuid);
+		}
 	}
 
-	public static class RequestAction implements IImmanenceAction {
+	public static class RequestAction extends ImmanenceAction {
+		protected int immanenceCount;
 
+		public RequestAction (UUID uuid, int immanenceCount) {
+			super(uuid);
+			this.immanenceCount = immanenceCount;
+		}
+
+		public int getImmanenceCount () {
+			return immanenceCount;
+		}
 	}
 
 	public static class JoinAction extends RequestAction {
-
+		public JoinAction (UUID uuid, int immanenceCount) {
+			super(uuid, immanenceCount);
+		}
 	}
 }
