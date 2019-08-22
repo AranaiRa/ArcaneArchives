6d57393dfd6d1dc981856f929db327fd6ee2bc65
AranaiRa <aranaiseraph@gmail.com>
2019-08-22 06:32:34 -0400

    Added Echoing Conformance Chamber with layout textures

--- src/main/java/com/aranaira/arcanearchives/blocks/EchoingConformanceChamber.java
+++ src/main/java/com/aranaira/arcanearchives/blocks/EchoingConformanceChamber.java
@@ -2,9 +2,11 @@ package com.aranaira.arcanearchives.blocks;
 
 import com.aranaira.arcanearchives.blocks.templates.BlockTemplate;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
@@ -28,5 +30,22 @@ public class EchoingConformanceChamber extends BlockTemplate {
 	public void addInformation (ItemStack stack, World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
 		tooltip.add(TextFormatting.RED + "" + TextFormatting.BOLD + I18n.format("arcanearchives.tooltip.notimplemented1"));
 		tooltip.add(TextFormatting.RED + "" + TextFormatting.ITALIC + I18n.format("arcanearchives.tooltip.notimplemented2"));
+	}
+
+	@Override
+	public BlockRenderLayer getRenderLayer () {
+		return BlockRenderLayer.CUTOUT;
+	}
+
+	@Override
+	@SuppressWarnings("deprecation")
+	public boolean isFullCube (IBlockState state) {
+		return false;
+	}
+
+	@Override
+	@SuppressWarnings("deprecation")
+	public boolean isOpaqueCube (IBlockState state) {
+		return false;
 	}
 }
