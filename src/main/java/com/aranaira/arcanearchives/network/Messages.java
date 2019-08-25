a348e56dbd9adf1c816428cc9e494f3f38aae2a2
Jon McManus <due@wxwhatever.com>
2019-08-25 16:54:57 +1000

    Fix up my packet fuck-up.

--- src/main/java/com/aranaira/arcanearchives/network/Messages.java
+++ src/main/java/com/aranaira/arcanearchives/network/Messages.java
@@ -11,6 +11,8 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.fml.common.network.ByteBufUtils;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class Messages {
@@ -109,27 +111,85 @@ public class Messages {
 		public void setValue (T value) {
 			this.value = value;
 		}
+	}
+
+	public static abstract class ConfigIntegerPacket extends ConfigPacket<Integer> {
+		public ConfigIntegerPacket () {
+		}
+
+		public ConfigIntegerPacket (Integer value) {
+			super(value);
+		}
 
 		@Override
-		@SuppressWarnings("unchecked")
 		public void fromBytes (ByteBuf buf) {
-			if (value.getClass() == Integer.class) {
-				setValue((T) (Integer) buf.readInt());
-			} else if (value.getClass() == Boolean.class) {
-				setValue((T) (Boolean) buf.readBoolean());
-			} else if (value.getClass() == String.class) {
-				setValue((T) ByteBufUtils.readUTF8String(buf));
-			}
+			setValue(buf.readInt());
 		}
 
 		@Override
 		public void toBytes (ByteBuf buf) {
-			if (value.getClass() == Integer.class) {
-				buf.writeInt((Integer) getValue());
-			} else if (value.getClass() == Boolean.class) {
-				buf.writeBoolean((Boolean) getValue());
-			} else if (value.getClass() == String.class) {
-				ByteBufUtils.writeUTF8String(buf, (String) getValue());
+			buf.writeInt(getValue());
+		}
+	}
+
+	public static abstract class ConfigBooleanPacket extends ConfigPacket<Boolean> {
+		public ConfigBooleanPacket () {
+		}
+
+		public ConfigBooleanPacket (Boolean value) {
+			super(value);
+		}
+
+		@Override
+		public void fromBytes (ByteBuf buf) {
+			setValue(buf.readBoolean());
+		}
+
+		@Override
+		public void toBytes (ByteBuf buf) {
+			buf.writeBoolean(getValue());
+		}
+	}
+
+	public static abstract class ConfigStringPacket extends ConfigPacket<String> {
+		public ConfigStringPacket () {
+		}
+
+		public ConfigStringPacket (String value) {
+			super(value);
+		}
+
+		@Override
+		public void fromBytes (ByteBuf buf) {
+			setValue(ByteBufUtils.readUTF8String(buf));
+		}
+
+		@Override
+		public void toBytes (ByteBuf buf) {
+			ByteBufUtils.writeUTF8String(buf, getValue());
+		}
+	}
+
+	public static abstract class ConfigStringArrayPacket extends ConfigPacket<String[]> {
+		public ConfigStringArrayPacket (String[] value) {
+			super(value);
+		}
+
+		@Override
+		public void fromBytes (ByteBuf buf) {
+			int count = buf.readInt();
+			List<String> values = new ArrayList<>();
+			for (int i = 0; i < count; i++) {
+				values.add(ByteBufUtils.readUTF8String(buf));
+			}
+			setValue(values.toArray(new String[0]));
+		}
+
+		@Override
+		public void toBytes (ByteBuf buf) {
+			buf.writeInt(value.length);
+			for (String s : value) {
+				ByteBufUtils.writeUTF8String(buf, s);
 			}
 		}
 	}
