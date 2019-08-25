a666d0ce6a96aaba5a28f3710a49a459c4c3cf96
Jon McManus <due@wxwhatever.com>
2019-08-25 11:25:09 +1000

    Temporarily disable shulker box functionality.
    It needs to be rewritten but there are other bug fixes to go out. See
     #53

--- src/main/java/com/aranaira/arcanearchives/tileentities/BrazierTileEntity.java
+++ src/main/java/com/aranaira/arcanearchives/tileentities/BrazierTileEntity.java
@@ -20,8 +20,6 @@ import net.minecraft.client.util.RecipeItemHelper;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.inventory.ItemStackHelper;
-import net.minecraft.item.ItemShulkerBox;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.NetworkManager;
@@ -220,18 +218,18 @@ public class BrazierTileEntity extends ImmanenceTileEntity implements IRanged {
 			toInsert.add(Objects.requireNonNull(playerInventory).extractItem(player.inventory.currentItem, item.getCount(), false));
 		}
 
-		boolean doShulkerThing = false;
-		NonNullList<ItemStack> itemList = NonNullList.withSize(999, ItemStack.EMPTY);
+		/*boolean doShulkerThing = false;
+		NonNullList<ItemStack> itemList = NonNullList.withSize(999, ItemStack.EMPTY);*/
 		List<ItemStack> remainder = new ArrayList<>();
 		NBTTagCompound tag = ItemUtils.getOrCreateTagCompound(item);
 
-		if (item.getItem() instanceof ItemShulkerBox && cap == null && tag.hasKey("BlockEntityTag")) {
+		/*if (item.getItem() instanceof ItemShulkerBox && cap == null && tag.hasKey("BlockEntityTag")) {
 			ItemStackHelper.loadAllItems(tag.getCompoundTag("BlockEntityTag"), itemList);
 			cap = new ShulkerItemHandler(itemList);
 			doShulkerThing = true;
-		}
+		}*/
 
-		if (cap != null) {
+		/*if (cap != null) {
 			for (int i = 0; i < cap.getSlots(); i++) {
 				ItemStack inSlot = cap.getStackInSlot(i);
 				int count = inSlot.getCount();
@@ -248,25 +246,25 @@ public class BrazierTileEntity extends ImmanenceTileEntity implements IRanged {
 				ItemStackHelper.saveAllItems(tag.getCompoundTag("BlockEntityTag"), ((ShulkerItemHandler) cap).getStacks(), false);
 				item.setTagCompound(tag);
 				remainder.add(item);
-			}
-		} else {
+			}*/
+		/*} else {*/
 
-			if (!doubleClick) {
-				playerToStackMap.put(player, item.copy());
-			} else {
-				// Collect all the items
-				for (int i = 0; i < playerInventory.getSlots(); i++) {
-					ItemStack inSlot = playerInventory.getStackInSlot(i);
-					if (ItemUtils.areStacksEqualIgnoreSize(inSlot, item)) {
-						toInsert.add(playerInventory.extractItem(i, inSlot.getCount(), false));
-					}
+		if (!doubleClick) {
+			playerToStackMap.put(player, item.copy());
+		} else {
+			// Collect all the items
+			for (int i = 0; i < playerInventory.getSlots(); i++) {
+				ItemStack inSlot = playerInventory.getStackInSlot(i);
+				if (ItemUtils.areStacksEqualIgnoreSize(inSlot, item)) {
+					toInsert.add(playerInventory.extractItem(i, inSlot.getCount(), false));
 				}
 			}
-
-			if (!toInsert.isEmpty()) {
-				remainder = InventoryRoutingUtils.tryInsertItems(this, item, toInsert);
-			}
 		}
+
+		if (!toInsert.isEmpty()) {
+			remainder = InventoryRoutingUtils.tryInsertItems(this, item, toInsert);
+		}
+		//}
 
 		boolean doUpdate = wasHeld;
 
