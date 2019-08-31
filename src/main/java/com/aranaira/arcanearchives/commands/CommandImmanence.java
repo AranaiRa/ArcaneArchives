61f7d218bca35725f922e770f1bc9852a96c5448
Jon McManus <due@wxwhatever.com>
2019-08-31 15:56:10 +1000

    Matrix Core is now an immanence generator.

--- src/main/java/com/aranaira/arcanearchives/commands/CommandImmanence.java
+++ src/main/java/com/aranaira/arcanearchives/commands/CommandImmanence.java
@@ -1,9 +1,13 @@
 package com.aranaira.arcanearchives.commands;
 
+import com.aranaira.arcanearchives.api.immanence.IImmanenceBus;
+import com.aranaira.arcanearchives.data.DataHelper;
+import com.aranaira.arcanearchives.data.ServerNetwork;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.text.TextComponentString;
 
 import java.util.Collections;
 import java.util.List;
@@ -36,6 +40,15 @@ public class CommandImmanence extends CommandBase {
 	public void execute (MinecraftServer server, ICommandSender sender, String[] args) {
 		if (sender instanceof EntityPlayer) {
 			EntityPlayer player = (EntityPlayer) sender;
+			ServerNetwork network = DataHelper.getServerNetwork(player.getUniqueID());
+			if (network == null) {
+				player.sendMessage(new TextComponentString("For some reason your network is null!"));
+				return;
+			}
+
+			IImmanenceBus bus = network.getImmanenceBus();
+			long diff = (System.currentTimeMillis() - bus.getLastTickTime()) / 1000;
+			player.sendMessage(new TextComponentString("Current base immanence is " + bus.getBaseImmanence() + ", multiplier " + bus.getMultiplier() + " for a total of " + bus.getTotalImmanence() + ". The last tick was " + diff + " seconds ago, and " + bus.getLeftoverImmanence() + " of the total remains."));
 		}
 	}
 }
--- src/main/java/com/aranaira/arcanearchives/tileentities/MatrixCoreTileEntity.java
+++ src/main/java/com/aranaira/arcanearchives/tileentities/MatrixCoreTileEntity.java
@@ -1,15 +1,20 @@
 package com.aranaira.arcanearchives.tileentities;
 
+import com.aranaira.arcanearchives.api.immanence.IImmanenceGenerator;
+import com.aranaira.arcanearchives.api.immanence.IImmanenceSource;
+import com.aranaira.arcanearchives.api.immanence.ImmanenceBonusType;
+import com.aranaira.arcanearchives.immanence.ImmanenceSource;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.play.server.SPacketUpdateTileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
+import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 
 @ParametersAreNonnullByDefault
-public class MatrixCoreTileEntity extends ImmanenceTileEntity {
+public class MatrixCoreTileEntity extends ImmanenceTileEntity implements IImmanenceGenerator {
 	public MatrixCoreTileEntity () {
 		super("matrix_core_tile_entity");
 		// immanenceGeneration = Integer.MAX_VALUE;
@@ -29,5 +34,11 @@ public class MatrixCoreTileEntity extends ImmanenceTileEntity {
 	@Override
 	public boolean shouldRefresh (World world, BlockPos pos, IBlockState oldState, IBlockState newSate) {
 		return (oldState.getBlock() != newSate.getBlock());
+	}
+
+	@Nullable
+	@Override
+	public IImmanenceSource generateImmanence () {
+		return new ImmanenceSource("matrix_core", 1000.0f, ImmanenceBonusType.ADDITIVE);
 	}
 }
