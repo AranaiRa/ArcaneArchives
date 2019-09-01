0599ab1ee69963d3f1cfa6a3c0b5ce2fde02fc49
Jon McManus <due@wxwhatever.com>
2019-09-01 10:28:44 +1000

    Clean up the Matrix Core tile entity.

--- src/main/java/com/aranaira/arcanearchives/tileentities/MatrixCoreTileEntity.java
+++ src/main/java/com/aranaira/arcanearchives/tileentities/MatrixCoreTileEntity.java
@@ -13,27 +13,9 @@ import net.minecraft.world.World;
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 
-@ParametersAreNonnullByDefault
 public class MatrixCoreTileEntity extends ImmanenceTileEntity implements IImmanenceGenerator {
 	public MatrixCoreTileEntity () {
 		super("matrix_core_tile_entity");
-		// immanenceGeneration = Integer.MAX_VALUE;
-	}
-
-	@Override
-	public void update () {
-
-		super.update();
-	}
-
-	@Override
-	public void onDataPacket (NetworkManager net, SPacketUpdateTileEntity pkt) {
-		super.onDataPacket(net, pkt);
-	}
-
-	@Override
-	public boolean shouldRefresh (World world, BlockPos pos, IBlockState oldState, IBlockState newSate) {
-		return (oldState.getBlock() != newSate.getBlock());
 	}
 
 	@Nullable
